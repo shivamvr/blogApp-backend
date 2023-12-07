3308e8955f18dd5bb9f7eb0bd471605e3d9a88ee
shivamvr <shivamweb2@gmail.com>
2023-12-07 20:35:01 +0530

    minor

--- model/post.model.js
+++ model/post.model.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const postSchema = mongoose.Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
+    data: {type: String},
     userId: String,
     name: String
 },{versionKey: false})
